390ceafc85cecf41fab794763f56151c361b813d
German Lagos <85660382+Geralexan@users.noreply.github.com>
2023-08-29 10:23:17 -0400

    Update problema3.py

--- problema3.py
+++ problema3.py
@@ -9,5 +9,35 @@
 #         20 30 90 90 8 5 90
 #La salida debe ser
 #         [20, [8, [5, [], [], []], [], []], [], [30, [], [], [90, [], [90, [], [90, [], [], []], []], []]]]
-t = input()
-print(t)
+def insertar_arbol_trinario(root, value):
+    if not root:
+        return [value, [], [], []]
+
+    if value < root[0]:
+        root[1] = insertar_arbol_trinario(root[1], value)
+    elif value == root[0]:
+        root[2] = insertar_arbol_trinario(root[2], value)
+    else:
+        root[3] = insertar_arbol_trinario(root[3], value)
+
+    return root
+
+
+def construir_arbol_trinario(numbers):
+    root = None
+    for num in numbers:
+        root = insertar_arbol_trinario(root, num)
+    return root
+
+
+def main():
+    t = input()
+    numeros = list(map(int, t.split()))
+
+    trinario_tree = construir_arbol_trinario(numeros)
+
+    print(trinario_tree)
+
+
+if __name__ == '__main__':
+    main()
